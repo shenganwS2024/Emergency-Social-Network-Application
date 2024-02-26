a82579f50deda0b25255bb24f047c8b0c2ebe72e
weemps <shengan2@illinois.edu>
2024-02-26 02:55:53 -0600

    seems to be working

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -6,12 +6,10 @@ const socketConfig = (server) => {
     const io = new Server(server);
 
     io.use((socket, next) => {
-        console.log("gimme token",socket.handshake.query.token)
         if (socket.handshake.query && socket.handshake.query.token !== 'null') {
           jwt.verify(socket.handshake.query.token, 'fsesb2secretkey', (err, decoded) => {
-            console.log("err",err)
+            //console.log("err",err)
             if (err) return next(new Error('Authentication error'));
-            console.log("whyhhhh")
             socket.decoded = decoded;
             next();
           });
--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 import {express,app,http,io} from './config/serverConfig.js'
 
 import { userRoutes, messageRoutes } from './config/index.js'
-import connectDB from './config/database.js';
+import DBConnection from './config/database.js';
 
 
 
 app.use(express.json());
 app.use(express.static('views'));
 
-connectDB();
+const DBUri = 'mongodb+srv://yus2:1111@fse.dty3o9p.mongodb.net/ESN';
+DBConnection.getInstance(DBUri).then((connection) => {
+    console.log('Database is connected');
+});
 
 //Configuration for session management
 app.use(userRoutes);
