89ecebb697db77b6f2f791fc4e67600f6cc6c397
SimonSun22 <84614775+SimonSun22@users.noreply.github.com>
2024-03-12 21:52:18 -0700

    add unit test for username and password

---
+++ test/passwordTest.test.js
@@ -0,0 +1,32 @@
+import { validateUserInfo } from '../controllers/userController';
+
+describe('Test Cases to validate password rule', () => {
+    it('Password is shorter than required', async () => {
+      const username = 'testuser';
+      const password = 'pas';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(false);
+    });
+
+    it('Password meets minimum length requirement', async () => {
+        const username = 'testuser';
+        const password = 'pass';
+        const result = await validateUserInfo(username, password);
+        expect(result).toBe(true);
+    });
+
+    it('Password meets minimum length with mixed characters', async () => {
+        const username = 'testuser';
+        const password = '! +%';
+        const result = await validateUserInfo(username, password);
+        expect(result).toBe(true);
+    });
+
+    it('Long and complicated valid password', async () => {
+        const username = 'testuser';
+        const password = '#$%@#$%gwergwervrewv  ewrfwerf$#@%5234543265363546wvse! +%fqwfw4ff';
+        const result = await validateUserInfo(username, password);
+        expect(result).toBe(true);
+    });
+
+});
---
+++ test/usernameTest.test.js
@@ -0,0 +1,60 @@
+import { validateUserInfo } from '../controllers/userController';
+
+// Use your test URI here
+//const TestDBUri = 'mongodb+srv://yus2:1111@fse.dty3o9p.mongodb.net/ESNTestDB';
+//
+//beforeAll(async () => {
+//    await DBConnection.getInstance(TestDBUri);
+//});
+
+
+describe('Test Cases to validate username rule', () => {
+    it('Valid username', async () => {
+        const username = 'testuser';
+        const password = 'password';
+        const result = await validateUserInfo(username, password);
+        expect(result).toBe(true);
+    });
+
+    it('Username is banned', async () => {
+      const username = 'admin';
+      const password = 'password';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(false);
+    });
+
+    it('Username is too short', async () => {
+      const username = 'ab';
+      const password = 'password';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(false);
+    });
+
+    it('Username is not case sensitive', async () => {
+      const username = 'ADMIN';
+      const password = 'password';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(false);
+    });
+
+    it('Valid username but complicated with symbols', async () => {
+      const username = '6345@#$%Rgwer@#$%#$@6WREGEWT#$@%#$Tfergwer#@$%@#$%#T';
+      const password = 'password';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(true);
+    });
+
+    it('Valid username with spaces', async () => {
+      const username = '    tes     tuse      r     ';
+      const password = 'password';
+      const result = await validateUserInfo(username, password);
+      expect(result).toBe(true);
+  });
+});
+
+//afterAll(async () => {
+//    // clean the database
+//    await mongoose.connection.db.dropDatabase();
+//    await DBConnection.close();
+//});
+
