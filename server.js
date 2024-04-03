8270db71b09e6cb66f1e15c4d5c43bc8b07fb3ca
weemps <shengan2@illinois.edu>
2024-04-03 00:11:46 -0500

    version updated

--- server.js
+++ server.js
@@ -8,33 +8,58 @@ import { nodeProfilingIntegration } from "@sentry/profiling-node";
 app.use(express.json());
 app.use(express.static('views'));
 
-Sentry.init({
-  dsn: "https://c7ff13503724cc1a1fbe24d1912d06bf@o4507020676431872.ingest.us.sentry.io/4507020687704064",
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Sentry.Integrations.Express({ app }),
-    nodeProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
-
-// The request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler());
-
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
 
 if (process.env.NODE_ENV !== 'test') {
-    DBConnection.getInstance(PRODUCTION_DB_URI).then((connection) => {
-    console.log('Database is connected');
-    });
-  }
-
+  Sentry.init({
+    dsn: "https://c7ff13503724cc1a1fbe24d1912d06bf@o4507020676431872.ingest.us.sentry.io/4507020687704064",
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+      nodeProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+  
+  // The request handler must be the first middleware on the app
+  app.use(Sentry.Handlers.requestHandler());
+  
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+  
+  if (process.env.NODE_ENV !== 'test') {
+      DBConnection.getInstance(PRODUCTION_DB_URI).then((connection) => {
+      console.log('Database is connected');
+      });
+    }
+  
+  
+  
+  // app.use(checkSpeedTestMode);
+  app.use(userRoutes);
+  app.use(messageRoutes);
+  app.use(speedTestRoutes);
+  app.use(annoucementRoutes);
+  app.use(searchRoutes);
+  
+  app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  
+  app.use(Sentry.Handlers.errorHandler());
+  
+  // Optional fallthrough error handler
+  app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+  });
+}
 
 
 // app.use(checkSpeedTestMode);
@@ -44,18 +69,5 @@ app.use(speedTestRoutes);
 app.use(annoucementRoutes);
 app.use(searchRoutes);
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-
-app.use(Sentry.Handlers.errorHandler());
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
 
 export {http,io, app, PRODUCTION_DB_URI, TEST_DB_URI};
