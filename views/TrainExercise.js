fba72aaf7510469602a915a782eaa6a051b077f0
Rongxin Tian <rongxint@andrew.cmu.edu>
2024-04-10 03:03:42 -0700

    Refactore more

--- views/TrainExercise.js
+++ views/TrainExercise.js
@@ -3,15 +3,16 @@ const socket = io('http://localhost:3000', {
     token: localStorage.getItem('token'),
   },
 })
+
 const backButton = document.getElementById('backButton')
 backButton.addEventListener('click', function () {
-  window.location.href = 'ESN Directory.html'
+  navigateTo('ESN Directory.html')
 })
 
 const exercisesList = document.getElementById('exercisesList')
 const uploadButton = document.getElementById('uploadButton')
 uploadButton.addEventListener('click', function () {
-  window.location.href = 'UploadExercise.html'
+  navigateTo('UploadExercise.html')
 })
 
 async function fetchExercises() {
@@ -27,30 +28,16 @@ async function fetchExercises() {
 function displayExercises(exercises) {
   exercisesList.innerHTML = ''
   exercises.forEach((exercise) => {
-    const exerciseCard = document.createElement('article')
-    exerciseCard.className = 'exercise-card'
-
-    const title = document.createElement('h2')
-    title.textContent = exercise.title
-
-    const userInfo = document.createElement('p')
-    userInfo.className = 'user-info'
-    userInfo.innerHTML = `${exercise.author} - <time datetime="${exercise.timestamp}">${new Date(
-      exercise.timestamp,
-    ).toLocaleString()}</time>`
-
-    exerciseCard.appendChild(title)
-    exerciseCard.appendChild(userInfo)
-
-    exerciseCard.addEventListener('click', () => {
-      window.location.href = `ExerciseDetails.html?id=${exercise._id}`
-    })
-
-    exercisesList.appendChild(exerciseCard)
+    addExerciseToDOM(exercise)
   })
 }
 
 function addExerciseToDOM(exercise) {
+  const exerciseCard = createExerciseCard(exercise)
+  exercisesList.appendChild(exerciseCard)
+}
+
+function createExerciseCard(exercise) {
   const exerciseCard = document.createElement('article')
   exerciseCard.className = 'exercise-card'
 
@@ -67,10 +54,14 @@ function addExerciseToDOM(exercise) {
   exerciseCard.appendChild(userInfo)
 
   exerciseCard.addEventListener('click', () => {
-    window.location.href = `ExerciseDetails.html?id=${exercise._id}`
+    navigateTo(`ExerciseDetails.html?id=${exercise._id}`)
   })
 
-  exercisesList.appendChild(exerciseCard)
+  return exerciseCard
+}
+
+function navigateTo(url) {
+  window.location.href = url
 }
 
 socket.on('new exercise', (exercise) => {
--- views/UploadExercise.js
+++ views/UploadExercise.js
@@ -1,116 +1,127 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('addQuestionBtn').addEventListener('click', function () {
-    const quizQuestionsContainer = document.getElementById('quizQuestionsContainer')
-    const questionDiv = document.createElement('div')
-    questionDiv.className = 'quizQuestion'
-    const questionInput = document.createElement('input')
-    questionInput.type = 'text'
-    questionInput.name = 'question'
-    questionInput.placeholder = 'Enter question'
-    questionDiv.appendChild(questionInput)
-    const answerInput = document.createElement('input')
-    answerInput.type = 'text'
-    answerInput.name = 'answer'
-    answerInput.placeholder = 'Enter answer'
-    questionDiv.appendChild(answerInput)
-    quizQuestionsContainer.appendChild(questionDiv)
-  })
-  var modal = document.getElementById('successModal')
-  var okBtn = document.getElementById('okBtn')
-
-  okBtn.onclick = function () {
-    window.location.href = 'TrainExercise.html'
-  }
-
-  document.getElementById('cancelBtn').addEventListener('click', function () {
-    window.location.href = 'TrainExercise.html'
-  })
-
-  document.getElementById('newExerciseForm').addEventListener('submit', async function (e) {
-    e.preventDefault()
-    const title = document.getElementById('title').value.trim()
-    const videoLink = document.getElementById('videoLink').value.trim()
-    const questionElements = document.querySelectorAll('.quizQuestion')
-    const author = localStorage.getItem('username')
-    const timestamp = Date.now()
-    let isValid = true
-
-    // Clear all notifications
-    clearAllNotifications()
-
-    // Validate title
-    if (title === '') {
-      showNotification('Please enter a title for the exercise.', 'titleNotification')
-      isValid = false
-    }
-
-    // Validate video link
-    if (!isValidYouTubeLink(videoLink)) {
-      showNotification('Please enter a valid YouTube video link.', 'videoLinkNotification')
-      isValid = false
-    }
-
-    // Validate quiz questions
-    if (questionElements.length === 0) {
-      showNotification('Please add at least one quiz question.', 'quizNotification')
-      isValid = false
-    }
-
-    // Validate quiz questions and answers
-    questionElements.forEach((questionDiv, index) => {
-      const question = questionDiv.querySelector('input[name="question"]').value.trim()
-      const answer = questionDiv.querySelector('input[name="answer"]').value.trim()
-      if (question === '' || answer === '') {
-        showNotification(`Please fill in the question and its answer.`, 'quizNotification')
-        isValid = false
-      }
-    })
-
-    const quizQuestions = Array.from(questionElements).map((questionDiv) => {
-      return {
-        question: questionDiv.querySelector('input[name="question"]').value.trim(),
-        answer: questionDiv.querySelector('input[name="answer"]').value.trim(),
-      }
-    })
-
-    if (!isValid) {
-      return
-    }
-
-    try {
-      const response = await fetch('/exercises', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, author, videoLink, timestamp, quizQuestions }),
-      })
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-
-      modal.style.display = 'block'
-    } catch (error) {
-      console.error('Error saving exercise:', error)
-    }
-  })
-
-  function clearAllNotifications() {
-    document.querySelectorAll('.notification').forEach((notification) => {
-      notification.textContent = ''
-      notification.classList.remove('show')
-    })
-  }
-
-  function showNotification(message, elementId) {
-    const notification = document.getElementById(elementId)
-    notification.textContent = message
-    notification.classList.add('show')
-  }
-
-  function isValidYouTubeLink(url) {
-    const youtubeLinkPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/
-    return youtubeLinkPattern.test(url)
-  }
+  setUpEventListeners()
 })
+
+function setUpEventListeners() {
+  document.getElementById('addQuestionBtn').addEventListener('click', addQuestion)
+  document.getElementById('cancelBtn').addEventListener('click', () => redirectTo('TrainExercise.html'))
+  document.getElementById('newExerciseForm').addEventListener('submit', submitExerciseForm)
+  document.getElementById('okBtn').onclick = () => redirectTo('TrainExercise.html')
+}
+
+function addQuestion() {
+  const quizQuestionsContainer = document.getElementById('quizQuestionsContainer')
+  const questionDiv = createQuestionDiv()
+  quizQuestionsContainer.appendChild(questionDiv)
+}
+
+function createQuestionDiv() {
+  const questionDiv = document.createElement('div')
+  questionDiv.className = 'quizQuestion'
+
+  const questionInput = createInput('question', 'Enter question')
+  questionDiv.appendChild(questionInput)
+
+  const answerInput = createInput('answer', 'Enter answer')
+  questionDiv.appendChild(answerInput)
+
+  return questionDiv
+}
+
+function createInput(name, placeholder) {
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.name = name
+  input.placeholder = placeholder
+  return input
+}
+
+async function submitExerciseForm(e) {
+  e.preventDefault()
+
+  const formData = getFormData()
+  if (!formData.isValid) {
+    return
+  }
+
+  try {
+    const response = await fetch('/exercises', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    showModal('successModal')
+  } catch (error) {
+    console.error('Error saving exercise:', error)
+  }
+}
+
+function getFormData() {
+  clearAllNotifications()
+  const title = document.getElementById('title').value.trim()
+  const videoLink = document.getElementById('videoLink').value.trim()
+  const author = localStorage.getItem('username')
+  const timestamp = Date.now()
+  let isValid = true
+
+  // Validation
+  if (title === '') {
+    showNotification('Please enter a title for the exercise.', 'titleNotification')
+    isValid = false
+  }
+
+  if (!isValidYouTubeLink(videoLink)) {
+    showNotification('Please enter a valid YouTube video link.', 'videoLinkNotification')
+    isValid = false
+  }
+
+  const questionElements = document.querySelectorAll('.quizQuestion')
+  if (questionElements.length === 0) {
+    showNotification('Please add at least one quiz question.', 'quizNotification')
+    isValid = false
+  }
+
+  const quizQuestions = Array.from(questionElements).map((questionDiv) => {
+    const question = questionDiv.querySelector('input[name="question"]').value.trim()
+    const answer = questionDiv.querySelector('input[name="answer"]').value.trim()
+    if (question === '' || answer === '') {
+      showNotification(`Please fill in the question and its answer.`, 'quizNotification')
+      isValid = false
+    }
+    return { question, answer }
+  })
+
+  return { title, author, videoLink, timestamp, quizQuestions, isValid }
+}
+
+function clearAllNotifications() {
+  document.querySelectorAll('.notification').forEach((notification) => {
+    notification.textContent = ''
+    notification.classList.remove('show')
+  })
+}
+
+function showNotification(message, elementId) {
+  const notification = document.getElementById(elementId)
+  notification.textContent = message
+  notification.classList.add('show')
+}
+
+function isValidYouTubeLink(url) {
+  const youtubeLinkPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/
+  return youtubeLinkPattern.test(url)
+}
+
+function showModal(modalId) {
+  const modal = document.getElementById(modalId)
+  modal.style.display = 'block'
+}
+
+function redirectTo(url) {
+  window.location.href = url
+}
