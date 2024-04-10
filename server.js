54553ac4ab57a1ff7f949d50752631161522b962
Rongxin Tian <rongxint@andrew.cmu.edu>
2024-04-10 04:09:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,63 +15,57 @@ import { nodeProfilingIntegration } from '@sentry/profiling-node'
 app.use(express.json())
 app.use(express.static('views'))
 
-if (process.env.NODE_ENV !== 'test') {
+// Common routes
+const commonRoutes = [userRoutes, messageRoutes, speedTestRoutes, annoucementRoutes, searchRoutes, exerciseRoutes]
+
+function initializeSentry() {
   Sentry.init({
     dsn: 'https://c7ff13503724cc1a1fbe24d1912d06bf@o4507020676431872.ingest.us.sentry.io/4507020687704064',
     integrations: [
-      // enable HTTP calls tracing
       new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
       new Sentry.Integrations.Express({ app }),
       nodeProfilingIntegration(),
     ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    tracesSampleRate: 1.0,
     profilesSampleRate: 1.0,
   })
 
-  // The request handler must be the first middleware on the app
   app.use(Sentry.Handlers.requestHandler())
-
-  // TracingHandler creates a trace for every incoming request
   app.use(Sentry.Handlers.tracingHandler())
+}
 
-  if (process.env.NODE_ENV !== 'test') {
-    DBConnection.getInstance(PRODUCTION_DB_URI).then((connection) => {
-      console.log('Database is connected')
-    })
-  }
+function connectToDatabase() {
+  DBConnection.getInstance(PRODUCTION_DB_URI).then(() => {
+    console.log('Database is connected')
+  })
+}
 
-  // app.use(checkSpeedTestMode);
-  app.use(userRoutes)
-  app.use(messageRoutes)
-  app.use(speedTestRoutes)
-  app.use(annoucementRoutes)
-  app.use(searchRoutes)
-  app.use(exerciseRoutes)
+function initializeRoutes() {
+  commonRoutes.forEach((route) => app.use(route))
+}
 
+function initializeSentryErrorHandling() {
   app.get('/debug-sentry', function mainHandler(req, res) {
     throw new Error('My first Sentry error!')
   })
 
   app.use(Sentry.Handlers.errorHandler())
 
-  // Optional fallthrough error handler
   app.use(function onError(err, req, res, next) {
-    // The error id is attached to `res.sentry` to be returned
-    // and optionally displayed to the user for support.
     res.statusCode = 500
     res.end(res.sentry + '\n')
   })
 }
 
-// app.use(checkSpeedTestMode);
-app.use(userRoutes)
-app.use(messageRoutes)
-app.use(speedTestRoutes)
-app.use(annoucementRoutes)
-app.use(searchRoutes)
-app.use(exerciseRoutes)
+if (process.env.NODE_ENV !== 'test') {
+  initializeSentry()
+  connectToDatabase()
+}
+
+initializeRoutes()
+
+if (process.env.NODE_ENV !== 'test') {
+  initializeSentryErrorHandling()
+}
 
 export { http, io, app, PRODUCTION_DB_URI, TEST_DB_URI }
